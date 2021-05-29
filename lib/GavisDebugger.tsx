0191a5734c9ea8679dc081ac9872bd11b5c5f67e
Jiseop Han <jshan2017@gmail.com>
2021-05-30 02:13:42 +0900

    auto scxroll

--- lib/GavisDebugger.tsx
+++ lib/GavisDebugger.tsx
@@ -3,7 +3,7 @@ import { logHistory } from "./LogHistory";
 import "./GavisDebugger.css";
 
 const GavisDebugger = (): JSX.Element => {
-  const [, setTicks] = useState(0);
+  const [ticks, setTicks] = useState(0);
 
   useEffect(() => {
     const intervalTimer = setInterval(
@@ -15,30 +15,39 @@ const GavisDebugger = (): JSX.Element => {
     };
   }, []);
 
+  useEffect(() => {
+    const historyElem = document.querySelector(".gavis-debugger-history");
+    document
+      .querySelector(".gavis-debugger-history")
+      ?.scrollTo(0, historyElem?.scrollHeight ?? 0);
+  }, [ticks]);
+
   return (
     <div className="gavis-debugger">
       <div className="gavis-debugger-title">Recent logger requests</div>
       <div className="gavis-debugger-buttons">
         <button onClick={logHistory.clear}>Clear</button>
       </div>
-      {logHistory.values.map(({ category, action, label, data, id }) => {
-        return (
-          <div key={id} className="gavis-debugger-item">
-            <li>
-              <b>Category</b>: {category}
-            </li>
-            <li>
-              <b>Action</b>: {action}
-            </li>
-            <li>
-              <b>Label</b>: {label}
-            </li>
-            <li>
-              <b>Data</b>: {data}
-            </li>
-          </div>
-        );
-      })}
+      <div className="gavis-debugger-history">
+        {logHistory.values.map(({ category, action, label, data, id }) => {
+          return (
+            <div key={id} className="gavis-debugger-item">
+              <li>
+                <b>Category</b>: {category}
+              </li>
+              <li>
+                <b>Action</b>: {action}
+              </li>
+              <li>
+                <b>Label</b>: {label}
+              </li>
+              <li>
+                <b>Data</b>: {data}
+              </li>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
